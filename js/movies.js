b0613890bfc6056f00f6c27966445abd19bcedf1
DESKTOP-6CU3LV8\jstel <jstella414@gmail.com>
2024-01-08 00:30:32 -0500

    added tv section

--- js/movies.js
+++ js/movies.js
@@ -8,7 +8,7 @@
   const movieTitle = document.querySelector("#movie-title-1")
   const movieRow = document.querySelector("#movie-row")
   const movieModal = document.querySelector("#movie-modal")
-
+  const tvRow = document.querySelector("#TV-row")
 
   const options = {
     method: 'GET',
@@ -153,5 +153,53 @@ const poptrendingside = (product) =>{
 }).catch(err => console.error(err));
 
 
+const shows = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGE1NjQzZjJlMTk1NmZhZGEwYzdhMDQ2NWQyZDUxMCIsInN1YiI6IjY1OTQ1ZjNkZTAwNGE2NmQ2MzE4NjAzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PgQOwPHjvAsLrlFJwMBMDqZv-xOOSJVUJKjOQfnV0G0'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc', shows)
+  .then(response => response.json())
+  .then(response =>{
+    const createRowTV = (product) =>{
+      tvRow.innerHTML += `
+      <div class="col-6 col-sm-6 col-lg-2  col-md-6 mt-3">
+            <div id = "movie-cards" class="card">
+              <img data-bs-toggle="modal" data-bs-target="#exampleModal" id ="image-posters" class = "img-fluid" width = "100%" src="https://image.tmdb.org/t/p/w500${product.poster_path}">
+              <div class="card-body">
+                <h5 id = "movie-title-1" class="card-title">${product.title}</h5>
+                <button id = "rating-button" type="button" class = "btn btn-light" disabled>${product.vote_average}</button>
+                <p class="card-text"></p>
+               
+              </div>
+            
+            </div>
+          </div>
+      `
+  }
+
+  for(product of response.results){
+    createRowTV(product);
+    console.log()
+  
+      }
+      
+      
+  
+    }
+    
+    
+    
+)
+  .catch(err => console.error(err));
+  
+
 
 })();
+
+
+
+
