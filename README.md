725c39d10c83fedacdf615433ac9274ba591ddb8
DESKTOP-6CU3LV8\jstel <jstella414@gmail.com>
2024-01-14 16:59:52 -0500

    fixed the filter!

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ Joseph Stella
 
 created a filter that works, biggest issue that i was not able to delete the full movie card but only the title. might look into finishing this feature.
 could not figure out how to get bootstrap modal to work. Sorry Jamal :/
+
+# notes update
+
+was able to get filter to work, realized the crud exercises had the answers i was looking for.
--- js/movies.js
+++ js/movies.js
@@ -4,16 +4,20 @@
 
 
 
-  const image1 = document.querySelector("#image-1")
-  const movieTitle = document.querySelector("#movie-title-1")
   const movieRow = document.querySelector("#movie-row")
-  const movieModal = document.querySelector("#movie-modal")
   const tvRow = document.querySelector("#TV-row")
 
-  ///filter targets
+  ///filter input targets
   const movieInput = document.querySelector("#Movie-Search-Type")
-  const movieoutput = document.querySelector("#user-display-search-text")
-
+  const tvInput = document.querySelector("#tv-Search-Type")
+  
+  
+  ////////
+  const trendingImageBig = document.querySelector("#trending-image-big")
+  const trendingDescription = document.querySelector("#trending-description")
+  const trendingMovieTitle = document.querySelector("#trending-movie-title")
+  const trendingMovierating = document.querySelector("#trending-movie-rating")
+  const trendingSide = document.querySelector("#trending-side")
 
 
 
@@ -29,74 +33,50 @@
     .then(response => response.json())
     .then(response => {
 
-      const createRow = (product) => {
+      response.results.forEach((product, i) => {
         movieRow.innerHTML += `
     
-      <div id = "poster-filter-div" class="col-6 col-sm-6 col-lg-2 col-md-4 mt-3 ">
-          <div id = "movie-cards" class="card">
-            <img data-bs-toggle="modal" data-bs-target="#exampleModal" id ="image-posters" class = "img-fluid" width = "100%" src="https://image.tmdb.org/t/p/w500${product.poster_path}">
-            <div class="card-body">
-              <h5 id = "movie-title" class="card-title">${product.title}</h5>
-              <button id = "rating-button" type="button" class = "btn btn-light btn-sm " disabled>User Rating: ${product.vote_average}</button>
-              <p class="card-text"></p>
-             
+        <div id = "poster-filter-div-${i}" class="col-6 col-sm-6 col-lg-2 col-md-4 mt-3 ">
+            <div id = "movie-cards" class="card">
+              <img data-bs-toggle="modal" data-bs-target="#exampleModal" id ="image-posters" class = "img-fluid" width = "100%" src="https://image.tmdb.org/t/p/w500${product.poster_path}">
+              <div class="card-body">
+                <h5 id = "movie-title" class="card-title">${product.title}</h5>
+                <button id = "rating-button" type="button" class = "btn btn-light btn-sm " disabled>User Rating: ${product.vote_average}</button>
+                <p class="card-text"></p>
+               
+              </div>
+            
             </div>
-          
-          </div>
-        </div>`
-
-      };
-
-      response.results.forEach((product, i) => {
-        createRow(product);
+          </div>`
       })
 
-
-      let posterFilterDiv = document.querySelectorAll("#poster-filter-div");
+      
+      
+      
       let titles = document.querySelectorAll("#movie-title");
 
+      // loop through movie titles
+      titles.forEach((title, i) => {
 
-      // loop through titles
-      titles.forEach((title) => {
+        const moviedivID = document.querySelector(`#poster-filter-div-${i}`)
+
         movieInput.addEventListener("input", (event) => {
-          if (title.textContent.toLowerCase().includes(movieInput.value)) {
-            title.style.display = 'block';
+          if (title.textContent.toLowerCase().includes(movieInput.value.toLowerCase())) {
+            moviedivID.style.display = 'block';
+
           } else {
-            title.style.display = 'none';
+            moviedivID.style.display = 'none';
 
           }
 
         })
       })
 
-      //loop through div    
-      posterFilterDiv.forEach((posterDiv, i) => {
-        movieInput.addEventListener("input", (event) => {
-
-
-
-
-
-
-        })
-      });
-
-
-
-      let posters = document.querySelectorAll("#image-posters")
-
-
-
     })
     .catch(err => console.error(err));
 
 
-  ////////
-  const trendingImageBig = document.querySelector("#trending-image-big")
-  const trendingDescription = document.querySelector("#trending-description")
-  const trendingMovieTitle = document.querySelector("#trending-movie-title")
-  const trendingMovierating = document.querySelector("#trending-movie-rating")
-  const trendingSide = document.querySelector("#trending-side")
+
 
 
   const trendingMovies = {
@@ -134,6 +114,7 @@
 
       }
 
+
       for (let i = 1; i < 4; i++) {
         poptrendingside(response.results[i])
 
@@ -153,27 +134,47 @@
   fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc', shows)
     .then(response => response.json())
     .then(response => {
-      const createRowTV = (product) => {
-        tvRow.innerHTML += `
-      <div class="col-6 col-sm-6 col-lg-2  col-md-4 mt-3">
-            <div id = "movie-cards" class="card TV">
-              <img data-bs-toggle="modal" data-bs-target="#exampleModal" id ="image-posters" class = "img-fluid" width = "100%" src="https://image.tmdb.org/t/p/w500${product.poster_path}">
-              <div class="card-body">
-                <h5 id = "movie-title-1" class="card-title">${product.name}</h5>
-                <button id = "rating-button" type="button" class = "btn btn-light" disabled>User Rating:${product.vote_average}</button>
-                <p class="card-text"></p>
-               
+ 
+      response.results.forEach((show, i)=>{
+        
+      tvRow.innerHTML += `
+        
+        <div id = "tv-poster-filter-div-${i}" class="col-6 col-sm-6 col-lg-2  col-md-4 mt-3">
+              <div id = "movie-cards" class="card TV">
+                <img data-bs-toggle="modal" data-bs-target="#exampleModal" id ="image-posters" class = "img-fluid" width = "100%" src="https://image.tmdb.org/t/p/w500${show.poster_path}">
+                <div class="card-body">
+                  <h5 id = "show-title" class="card-title">${show.name}</h5>
+                  <button id = "rating-button" type="button" class = "btn btn-light" disabled>User Rating:${show.vote_average}</button>
+                  <p class="card-text"></p>
+                 
+                </div>
+              
               </div>
-            
             </div>
-          </div>
-      `
+        `
+      })
 
-      }
 
-      for (product of response.results) {
-        createRowTV(product);
-      }
+      const tvTitles = document.querySelectorAll("#show-title") 
+      // loop through show titles
+
+      tvTitles.forEach((title, i) => {
+        
+        const tvbdivID = document.querySelector(`#tv-poster-filter-div-${i}`)
+
+        tvInput.addEventListener("input", (event) => {
+          if (title.textContent.toLowerCase().includes(tvInput.value.toLowerCase())) {
+            tvbdivID.style.display = 'block';
+
+          } else {
+            tvbdivID.style.display = 'none';
+
+          }
+
+        })
+      })
+
+     
     }
 
 
